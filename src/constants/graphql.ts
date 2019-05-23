564528862cb507a1999e5ab742ed9180130a3dde
Kris Steigerwald <kris.steigerwald@gmail.com>
2019-05-23 08:51:37 -0400

    update login/logout handling

--- src/constants/graphql.ts
+++ src/constants/graphql.ts
@@ -1,25 +1,38 @@
 import fetch from 'unfetch'
 import gql from 'graphql-tag'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client';
 import { createHttpLink  } from "apollo-link-http";
 import * as config from '../helpers/config';
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from 'apollo-link-context'
+let jwt_decode = require('jwt-decode')
 
-let jsonKey: any = localStorage.getItem(config.localKey('user')) || ''
 let GRAPH_QL_API = process.env.GRAPH_QL_API || 'https://hasura-velaru.herokuapp.com/v1alpha1/graphql'
 
-//const headers = { 'x-hasura-admin-secret' : 'Winter10' }
-const headers = { 'Authorization' : 'Bearer ' + jsonKey }
+var isAuthenticated = (expiresAt: any) => new Date().getTime() < expiresAt
 
-const link = createHttpLink({
+//const headers = { 'x-hasura-admin-secret' : 'Winter10' }
+//const headers = { 'Authorization' : 'Bearer ' + jsonKey }
+const authLink = setContext((_, {headers} ) => {
+    let token: any = localStorage.getItem(config.localKey('user')) || ''
+    let decode = jwt_decode(token)
+    if(!isAuthenticated) 
+        window.location = '/logout'
+    //console.log(decode)
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        } 
+    }
+})
+const httpLink = createHttpLink({
     uri: GRAPH_QL_API,
-    headers: headers
 })
 
 export const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-    uri: GRAPH_QL_API,
-    headers: headers
 })
 
 
