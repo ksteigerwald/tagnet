743e483a0abe5bf22a5f027c8bb1a91e527bf532
Kris Steigerwald <kris.steigerwald@gmail.com>
2019-06-22 08:55:07 -0400

    comment out test once again

--- tests/unit/home.spec.ts
+++ tests/unit/home.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 
+// "test:unit": "vue-cli-service test:unit"
 describe('Home.vue', () => {
   it('renders props.msg when passed', () => {
     const wrapper = shallowMount(Home, {
