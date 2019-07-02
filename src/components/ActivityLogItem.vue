b2821f755441d55899117768e4cd195f923b2a28
Kris Steigerwald <kris.steigerwald@gmail.com>
2019-07-02 18:58:59 -0400

    icons and such for log

--- src/components/ActivityLogItem.vue
+++ src/components/ActivityLogItem.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="activity-one">
+  <div :class="css">
     <ul class="goals-activity">
-            <li><span class="icon-activity"><img src="@/assets/images/gol-two.svg" alt=""></span></li>
+            <li><span class="icon-activity">
+              <Search v-if="icon === 0" />
+              <Goal v-if="icon === 1" />
+              <Event v-if="icon === 2" />
+              <Idea v-if="icon === 3" />
+              <Any v-if="icon === 4" />
+              <Topic v-if="icon === 5" />
+              <Person v-if="icon === 6" />
+              </span></li>
             <li><span class="line-box"></span></li>
-   </ul>
+  </ul>
     <div class="activity-conatnt">
       <h4>{{heading}}</h4>
       <p>{{text}}</p>
@@ -14,16 +22,67 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
+import Search from '@/components/Icons/Search.vue'
+import Goal from '@/components/Icons/Goal.vue'
+import Event from '@/components/Icons/Event.vue'
+import Idea from '@/components/Icons/Idea.vue'
+import Any from '@/components/Icons/Any.vue'
+import Topic from '@/components/Icons/Topic.vue'
+import Person from '@/components/Icons/Person.vue'
+
 import { State, Getter, Action, namespace } from 'vuex-class';
 import { Fact, FactState } from '@/types'
 import { create } from 'domain';
 
-@Component
+@Component({
+  components:{
+    Search,
+    Goal,
+    Event,
+    Idea,
+    Any,
+    Topic,
+    Person,
+  }
+})
 export default class ActivityLog extends Vue {
     
   @Prop() log: Fact
-//  heading: string = ''
-
+  icon: number = 0
+  css:string = 'activity-one'
+  cssRoot: string = 'activity-one'
+  beforeMount() {
+    switch(this.log.FactMemo.TagMemo.label) {
+      case 'goal':
+        this.icon = 1
+        this.css = `${this.cssRoot}`
+        break
+      case 'event':
+        this.icon = 2
+        this.css = `${this.cssRoot} activity-two`
+        break
+      case 'idea':
+        this.icon = 3
+        this.css = `${this.cssRoot} activity-three`
+        break
+      case 'any':
+        this.icon = 4
+        this.css = `${this.cssRoot} activity-four`
+        break
+      case 'topic':
+        this.icon = 5
+        this.css = `${this.cssRoot} activity-five`
+        break
+      case 'person':
+        this.icon = 6
+        this.css = `${this.cssRoot} activity-six`
+        break
+      default:
+        this.icon = 0
+        this.css = `${this.cssRoot}`
+        break
+    }
+  }
   get text() {
     if(this.log.search) return this.log.search
     if(this.log.line_id) return `[+] ${this.log.FactLine.label}`
---
+++ src/components/Icons/Search.vue
@@ -0,0 +1,18 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+  <g id="Group_358" data-name="Group 358" transform="translate(-1053 -1039)">
+    <circle id="Ellipse_105" data-name="Ellipse 105" cx="24" cy="24" r="24" transform="translate(1053 1039)" fill="#e5e5e5" opacity="0.3"/>
+    <path id="Path_15" data-name="Path 15" d="M42.719,76.135a7.545,7.545,0,1,0-2.541-2.541L37.59,76.182h0l-2.57,2.57a14.777,14.777,0,0,0,2.541,2.541l5.158-5.158Zm3.9-10.391a3.944,3.944,0,1,1-3.944,3.944A3.944,3.944,0,0,1,46.624,65.744Z" transform="translate(1031.98 990.85)" fill="#c3c3c3"/>
+  </g>
+</svg>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Search extends Vue {}
+</script>
+
+<style scoped lang="scss">
+</style>
