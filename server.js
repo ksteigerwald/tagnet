9c3196a686ff34e671f4b70568ae60244c898c63
Kris Steigerwald <kris.steigerwald@gmail.com>
2020-02-21 17:40:31 -0500

    logging

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
+console.log('SOMELOGGING');
+
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
